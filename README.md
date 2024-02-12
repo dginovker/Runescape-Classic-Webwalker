30d73d97a783bc8d653b69178c55c1510d069a2f
dginovker <danielginovker@gmail.com>
2024-02-12 18:44:47 +0900

    Add README, LICENSE, ability to delete edges/nodes, improve UX and cleanup code

---
+++ README.md
@@ -0,0 +1,30 @@
+# RSC Webwalker
+
+![Image showing nodes around Gnome Stronghold entrance](https://i.imgur.com/J0m93lq.png)
+
+GUI program for generating a `graph.json` of traversable paths around Runescape Classic.
+
+Very useful for writing highly efficient bot walking APIs that can go anywhere in the map.
+
+## Features
+
+* Load the RSC map (that was shamelessly stolen from APOS)
+* Load the existing graph.json
+* Plant nodes and edges
+* Label edges (in case you need custom code besides just "walkTo" to go between two nodes, i.e open a Gate, use a Ladder)
+
+## Hotkeys
+
+* Arrowkeys to move around the map
+* `+` and `-` to zoom
+* `Esc` to deselect the currently selected node
+
+## How to run
+
+* `pip install -r requirements.txt`
+* `python webwalk_editor.py`
+
+## How to use with a Bot Client
+
+* I plan to add this manually to APOS and IdleRSC. Simply use their `walkTowards` API method (todo)
+* If you want to integrate this manually, 1. Find a node in the graph you can walk to from your start 2. Astar on `graph.json` until you get close to the end 3. Walk to your end position. You will also need to implement custom handlers for all the labeled edges, since those edges probably need custom logic
--- webwalk_editor.py
+++ webwalk_editor.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import simpledialog  # Correctly import simpledialog
+from tkinter import simpledialog, messagebox  # Import messagebox for confirmations
 from PIL import Image, ImageTk
 import json
 import os
@@ -53,9 +53,7 @@ class ImageEditor:
         y = (y - self.offset_y) * self.zoom_factor
         if 0 <= x <= 400 and 0 <= y <= 400:
             color = "green" if selected else "red"
-            self.canvas.create_oval(
-                x - 5, y - 5, x + 5, y + 5, fill=color, outline=""
-            )
+            self.canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill=color, outline="")
 
     def draw_edge(self, edge):
         start, end = edge[0], edge[1]
@@ -68,10 +66,59 @@ class ImageEditor:
             (end[1] - self.offset_y) * self.zoom_factor,
         )
         if all(0 <= v <= 400 for v in [start_x, start_y, end_x, end_y]):
-            line = self.canvas.create_line(start_x, start_y, end_x, end_y, fill="red", width=2)
-            edge_key = str(edge[0]) + "-" + str(edge[1])
+            line = self.canvas.create_line(
+                start_x, start_y, end_x, end_y, fill="red", width=2
+            )
+            edge_key = self.make_edge_key(edge[0], edge[1])
             if edge_key in self.edge_labels:
-                self.canvas.create_text((start_x + end_x) / 2, (start_y + end_y) / 2, text=self.edge_labels[edge_key], fill="blue")
+                self.canvas.create_text(
+                    (start_x + end_x) / 2,
+                    (start_y + end_y) / 2,
+                    text=self.edge_labels[edge_key],
+                    fill="blue",
+                )
+
+    def create_or_edit_edge(self, node_from, node_to, prompt_for_label=True):
+        edge = [node_from, node_to]
+        edge_reversed = [node_to, node_from]
+        edge_key = self.make_edge_key(node_from, node_to)
+        if edge not in self.graph["edges"] and edge_reversed not in self.graph["edges"]:
+            self.graph["edges"].append(edge)
+            return True
+        elif prompt_for_label and edge_key in self.edge_labels:
+            self.prompt_edge_label(edge)
+        return False
+
+    def prompt_edge_label(self, edge):
+        edge_key = self.make_edge_key(edge[0], edge[1])
+        existing_label = self.edge_labels.get(edge_key, "")
+        label = simpledialog.askstring(
+            "Label Edge",
+            "Enter label for this edge:",
+            initialvalue=existing_label,
+            parent=self.master,
+        )
+        if label is not None:  # Check for Cancel click
+            self.edge_labels[edge_key] = label
+        else:
+            if messagebox.askyesno("Delete Edge", "Do you want to delete this edge?"):
+                self.delete_edge(edge)
+
+    def delete_node(self, node):
+        if messagebox.askyesno(
+            "Delete Node", "Do you want to delete this node and its connected edges?"
+        ):
+            self.graph["nodes"].remove(node)
+            # Remove connected edges and labels
+            edges_to_remove = [edge for edge in self.graph["edges"] if node in edge]
+            for edge in edges_to_remove:
+                self.graph["edges"].remove(edge)
+                edge_key = self.make_edge_key(edge[0], edge[1])
+                if edge_key in self.edge_labels:
+                    del self.edge_labels[edge_key]
+
+            self.selected_node = None
+            self.redraw()  # Ensure the canvas is updated immediately after deletion
 
     def on_click(self, event):
         x, y = (event.x / self.zoom_factor + self.offset_x), (
@@ -79,26 +126,44 @@ class ImageEditor:
         )
         coords = (int(x), int(y))
         closest_node, distance = self.find_closest_node(coords)
+
         if distance <= 9:  # Click is within 3 tiles of an existing node
-            if self.selected_node and self.selected_node != closest_node:
-                edge = [self.selected_node, closest_node]
-                self.graph["edges"].append(edge)
-                self.request_edge_label(edge)
-            self.selected_node = closest_node  # Select the closest node
+            if self.selected_node == closest_node:
+                self.delete_node(closest_node)  # Delete node if it's the selected one
+            elif self.selected_node and self.selected_node != closest_node:
+                if not self.create_or_edit_edge(
+                    self.selected_node, closest_node, prompt_for_label=False
+                ):
+                    self.prompt_edge_label([self.selected_node, closest_node])
+                self.selected_node = closest_node  # Update selected node
+            else:
+                self.selected_node = closest_node
         else:
             self.graph["nodes"].append(coords)
             if self.selected_node:
-                edge = [self.selected_node, coords]
-                self.graph["edges"].append(edge)
-                self.request_edge_label(edge)
+                self.create_or_edit_edge(
+                    self.selected_node, coords, prompt_for_label=False
+                )
             self.selected_node = coords  # Automatically select the new node
-        self.redraw()  # Ensure UI is refreshed
 
-    def request_edge_label(self, edge):
-        label = simpledialog.askstring("Label Edge", "Enter label for this edge:", parent=self.master)
-        if label:
-            edge_key = str(edge[0]) + "-" + str(edge[1])
-            self.edge_labels[edge_key] = label
+        self.redraw()  # Redraw after any click action to ensure the canvas is up-to-date
+
+    def delete_edge(self, edge):
+        edge_key = self.make_edge_key(edge[0], edge[1])
+        if edge in self.graph["edges"]:
+            self.graph["edges"].remove(edge)
+        elif [edge[1], edge[0]] in self.graph[
+            "edges"
+        ]:  # Remove reversed edge if present
+            self.graph["edges"].remove([edge[1], edge[0]])
+        if edge_key in self.edge_labels:
+            del self.edge_labels[edge_key]
+        self.redraw()
+
+    def make_edge_key(self, node1, node2):
+        # Sort nodes to treat edges as undirected
+        sorted_nodes = sorted([node1, node2], key=lambda x: (x[0], x[1]))
+        return str(sorted_nodes[0]) + "-" + str(sorted_nodes[1])
 
     def update_coordinates(self, event):
         x, y = (event.x / self.zoom_factor + self.offset_x), (
@@ -107,7 +172,9 @@ class ImageEditor:
         self.label.config(text=f"Coordinates: ({int(x)}, {int(y)})")
 
     def on_key(self, event):
-        key = event.keysym
+        self.handle_navigation(event.keysym)
+
+    def handle_navigation(self, key):
         step = 30 / self.zoom_factor
         zoom_step = 0.1
         if key in ["Right", "6"]:
@@ -127,9 +194,14 @@ class ImageEditor:
         self.redraw()
 
     def save_graph(self):
-        save_data = {"nodes": self.graph["nodes"], "edges": self.graph["edges"], "labels": self.edge_labels}
+        save_data = {
+            "nodes": self.graph["nodes"],
+            "edges": self.graph["edges"],
+            "labels": self.edge_labels,
+        }
         with open("graph.json", "w") as f:
             json.dump(save_data, f)
+        messagebox.showinfo("Save", "Graph saved successfully.")
 
     def load_graph(self):
         if os.path.exists("graph.json"):
@@ -140,7 +212,7 @@ class ImageEditor:
 
     def find_closest_node(self, coords):
         closest_node = None
-        min_distance = float('inf')
+        min_distance = float("inf")
         for node in self.graph["nodes"]:
             distance = (node[0] - coords[0]) ** 2 + (node[1] - coords[1]) ** 2
             if distance < min_distance:
